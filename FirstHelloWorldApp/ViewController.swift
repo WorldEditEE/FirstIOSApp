997212c73d99f246a04f0cf34d1f5d3ba4ac7794
aleksandrukolov <sanya_armeitsev@mail.ru>
2022-01-26 17:14:53 +0300

    @feature Добавлена возможность скрытия и показа текста

--- FirstHelloWorldApp/ViewController.swift
+++ FirstHelloWorldApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var changeTextButton: UIButton!
+    @IBOutlet var labelHelloWorld: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelHelloWorld.isHidden = true
+        
     }
 
-
+    @IBAction func changeTextAction(_ sender: Any) {
+        
+        if labelHelloWorld.isHidden {
+            labelHelloWorld.isHidden = false
+            changeTextButton.setTitle("Скрыть текст", for: .normal)
+        } else {
+            labelHelloWorld.isHidden = true
+            changeTextButton.setTitle("Показать текст", for: .normal)
+        }
+        
+    }
+    
 }
 
